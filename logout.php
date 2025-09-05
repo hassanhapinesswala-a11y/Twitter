67763388548639efec03dfb5dd77ae11a2848865
Hassan happiness wala <hassanhapinesswala@gmail.com>
2025-09-05 11:48:17 +0500

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,4 @@
+<?php require 'db.php'; session_destroy(); ?>
+<!DOCTYPE html><html><head><meta charset="utf-8"><title>Logging out…</title></head><body>
+<script>location.href='login.php'</script>
+</body></html>
